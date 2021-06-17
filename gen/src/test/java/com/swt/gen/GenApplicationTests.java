4a5b41f4288814626af97e2e0eebf9d11f596dbf
shaowentao <1658870477@qq.com>
2021-06-17 16:46:00 +0800

    gen

---
+++ gen/src/test/java/com/swt/gen/GenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swt.gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
